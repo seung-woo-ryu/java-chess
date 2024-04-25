2262d2715478c35e6da25736765856b2085b6c64
seung-woo-ryu <tmddn645@naver.com>
2024-04-25 22:57:15 +0900

    feat: add getter() in Position

--- src/main/java/Model/position/Position.java
+++ src/main/java/Model/position/Position.java
@@ -5,8 +5,20 @@ public class Position {
     private final Row row;
     private final Column column;
 
+    public Row getRow() {
+        return row;
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
     public Position(Row row, Column column) {
         this.row = row;
         this.column = column;
+    }
+
+    public boolean isSameRow(int firstRow) {
+        return this.row.getValue() == firstRow;
     }
 }
